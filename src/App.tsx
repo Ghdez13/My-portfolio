fc5198c8e63daf9478aa796317d66ab57124986c
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2025-11-21 23:00:38 -0500

    Create color palette for light and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <h1 id="home-heading" className="text-3xl font-bold">
             Home
           </h1>
-          <p>
+          <p className="text-(--color-text-primary)">
             Explore Jerry's expertise in Java, Spring Boot, microservices, and
             scalable backend architecture.
           </p>
